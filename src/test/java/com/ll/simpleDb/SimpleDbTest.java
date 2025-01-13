3d63135fc700a085c74af309d6cf37be06ec9b17
skvhffpdyd <115456416+skvhffpdyd@users.noreply.github.com>
2025-01-13 14:46:26 +0900

    init

---
+++ src/test/java/com/ll/simpleDb/SimpleDbTest.java
@@ -0,0 +1,43 @@
+package com.ll.simpleDb;
+
+import org.junit.jupiter.api.*;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class SimpleDbTest {
+    private static SimpleDb simpleDb;
+
+    @BeforeAll
+    public static void beforeAll() {
+        simpleDb = new SimpleDb("localhost", "root", "lldj123414", "simpleDb__test");
+        simpleDb.setDevMode(true);
+
+        createArticleTable();
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+    //    truncateArticleTable();
+    //    makeArticleTestData();
+    }
+
+    @Test
+    void t000() {
+
+    }
+
+    private static void createArticleTable() {
+        simpleDb.run("DROP TABLE IF EXISTS article");
+
+        simpleDb.run("""
+                CREATE TABLE article (
+                    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                    PRIMARY KEY(id),
+                    createdDate DATETIME NOT NULL,
+                    modifiedDate DATETIME NOT NULL,
+                    title VARCHAR(100) NOT NULL,
+                    `body` TEXT NOT NULL,
+                    isBlind BIT(1) NOT NULL DEFAULT 0
+                )
+                """);
+    }
+}
